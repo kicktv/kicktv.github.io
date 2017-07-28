e2e36018a46bc859a791abe84b1f3814d563c511
khaledtv <khaledtv29@gmail.com>
2017-07-28 23:26:59 +0000

    Create save.js

---
+++ save.js
@@ -0,0 +1,13 @@
+var options = { progress: function (progress) {
+        var value = Math.round(100*progress);
+        $('.progress').css('display', 'inherit');
+        $('.progress-bar').css('width', value+'%').attr('aria-valuenow', value);}};
+$("#btn_save").click(function() {
+    var url = $('#url').val();
+    var filename = $('#name').val();
+    Dropbox.save(url, filename, options);});
+$("#url").on("propertychange change keyup paste input", function(){
+    var url = $('#url').val();
+    if(url.indexOf("/") != -1){
+        $('#name').val(url.substring(url.lastIndexOf("/")+1));}
+else{$('#name').val(url);}});
