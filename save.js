1fada39b42d3caaa5fd50e8d0b62c53c180e2a5d
khaledtv <khaledtv29@gmail.com>
2018-08-11 01:01:20 +0100

    Update save.js

--- save.js
+++ save.js
@@ -1,13 +1,1 @@
-var options = { progress: function (progress) {
-        var value = Math.round(100*progress);
-        $('.progress').css('display', 'inherit');
-        $('.progress-bar').css('width', value+'%').attr('aria-valuenow', value);}};
-$("#btn_save").click(function() {
-    var url = $('#url').val();
-    var filename = $('#name').val();
-    Dropbox.save(url, filename, options);});
-$("#url").on("propertychange change keyup paste input", function(){
-    var url = $('#url').val();
-    if(url.indexOf("/") != -1){
-        $('#name').val(url.substring(url.lastIndexOf("/")+1));}
-else{$('#name').val(url);}});
+var options={progress:function(a){var n=Math.round(100*a);$(".progress").css("display","inherit"),$(".progress-bar").css("width",n+"%").attr("aria-valuenow",n)}};$("#btn_save").click(function(){var a=$("#url").val(),n=$("#name").val();Dropbox.save(a,n,options)}),$("#url").on("propertychange change keyup paste input",function(){var a=$("#url").val();-1!=a.indexOf("/")?$("#name").val(a.substring(a.lastIndexOf("/")+1)):$("#name").val(a)});
